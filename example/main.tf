e31fc77fd327950f7246ff9a16267fc65759b197
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-02-12 23:22:18 +0900

    create network

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,14 @@
-module "web_server" {
-  source = "./http_server"
-  instance_type ="t3.micro"
+module "describe_regions_for_ec2" {
+  source = "./iam_role"
+  name="describe-regions-for-ec2"
+  identifier="ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.allow_describe_regions.json
 }
 
-output "public_dns" {
-  value = module.web_server.public_dns
+data "aws_iam_policy_document" "allow_describe_regions" {
+  statement {
+    effect="Allow"
+    actions=["ec2:DescribeRegions"]
+    resources=["*"]
+  }
 }
