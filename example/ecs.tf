55cbea8f3909cdd99c45f1485079f1bd704aa566
尾崎しょうご <syougo1999most@yahoo.co.jp>
2022-02-16 16:23:59 +0900

    create datastore

--- example/ecs.tf
+++ example/ecs.tf
@@ -7,7 +7,7 @@ resource "aws_ecs_task_definition" "example" {
   memory="512"
   network_mode ="awsvpc"
   requires_compatibilities = ["FARGATE"]
-  container_definitions =file("./container_definitions.json")
+  container_definitions =file("./task_definitions/container_definitions.json")
   execution_role_arn = module.ecs_task_execution_role.iam_role_arn
 }
 
